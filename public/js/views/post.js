cda8f45f04d68ee386ec2bbc247274ceafb6e639
David Daza <03dazal@gmail.com>
2018-08-06 11:57:19 -0500

    Agrega disqus

--- public/js/views/post.js
+++ public/js/views/post.js
@@ -19,3 +19,16 @@ function getLayoutClass () {
 window.addEventListener('resize', setClass);
 
 setClass();
+
+/* eslint no-unused-vars: off */
+const disqus_config = function () {
+	this.page.url = window.location.href;
+	this.page.identifier = window.location.href;
+	this.language = 'es_MX';
+};
+(function () {
+	const s = document.createElement('script');
+	s.src = '//datasketch.disqus.com/embed.js';
+	s.setAttribute('data-timestamp', +new Date());
+	document.body.appendChild(s);
+})();
