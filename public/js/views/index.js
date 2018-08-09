2920eb0b591f97cd788aa5158c45b9e0b57530c0
David Daza <03dazal@gmail.com>
2018-08-09 11:10:52 -0500

    Add sponsored banner

--- public/js/views/index.js
+++ public/js/views/index.js
@@ -1,8 +1,23 @@
 /* eslint no-undef: off*/
+const moveTo = new MoveTo({ tolerance: 20 });
+const chevron = document.getElementById('chevron');
+
+moveTo.registerTrigger(chevron);
+
 const sheet = 'Lideres sociales asesinados desde la firma del pro';
+const keys = {
+	leaders: 'https://docs.google.com/spreadsheets/d/1I9NHXhAPSzruE5s94_X5FQpRMksz6MTHgLCgrTviqfM/edit#gid=0',
+	calendar: 'https://docs.google.com/spreadsheets/d/10Pr1OU9XMq__YpuRCEE0Smza6nbrEmuuNeUTT-XEuEU/edit#gid=338236224',
+};
+
 Tabletop.init({
-	key: 'https://docs.google.com/spreadsheets/d/1I9NHXhAPSzruE5s94_X5FQpRMksz6MTHgLCgrTviqfM/edit#gid=0',
+	key: keys.leaders,
 	callback: getTotal,
+});
+
+Tabletop.init({
+	key: keys.calendar,
+	callback: getCalendar,
 });
 
 function getTotal (data, tabletop) {
@@ -16,8 +31,20 @@ function getTotal (data, tabletop) {
 	}
 }
 
-const moveTo = new MoveTo({ tolerance: 20 });
-const chevron = document.getElementById('chevron');
-moveTo.registerTrigger(chevron);
+function getCalendar (data, tabletop) {
+	const calendar = tabletop.sheets('eventos-lideres').all().filter(date => date.FECHA.match(/^\d/));
+	const template = calendar.reduce((str, event) => {
+		const place = event['LUGAR DE REALIZACIÓN']
+			? `${event['LUGAR DE REALIZACIÓN'].trim()}, ${event.CIUDAD}`
+			: event.CIUDAD;
+		str += `<div class="calendar__event column">
+			<span>${event.NOMBRE}</span>
+			<span><small><i class="fa fa-calendar"></i> ${event.FECHA.replace(/\-/g, '/')}</small></span>
+			<span><small><i class="fa fa-map-marker"></i> ${place}</small></span>
+		</div>`;
+		return str;
+	}, '');
+	document.querySelector('.calendar__events').innerHTML = template;
+}
 
 iFrameResize({}, '#iframe');
